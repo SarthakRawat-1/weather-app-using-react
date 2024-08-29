2bae3d1b2d734c2f452c44ee81f1c88bc4914bf1
SarthakRawat-1 <sarthakrawat525@gmail.com>
2024-08-30 01:51:35 +0530

    Revert "second commit"
    This reverts commit a3d6e32c4bd45e5a342d4d424a74ed03d5b871af.

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,7 +1,7 @@
 function WeatherIcon({ iconNumber, summary }) {
   return (
     <img
-      src={`/public/dist/weather_icons/set04/big/${iconNumber}.png`}
+      src={`/dist/weather_icons/set04/big/${iconNumber}.png`}
       alt={summary}
       draggable={false}
     />
